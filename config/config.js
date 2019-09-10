ea911034a0d0ebfb1c342d96e1a377b52474cf1b
aduka <aduka@alteryx.com>
2019-09-10 19:49:33 +0300

    db config from env vars

--- config/config.js
+++ config/config.js
@@ -1,4 +1,7 @@
-{
+
+
+
+module.exports = {
   "development": {
     "username": "kLtnk2xqwN",
     "password": "Lw8Zr7l2Dv",
@@ -17,11 +20,11 @@
     "operatorsAliases": false
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": process.env.SQL_DB_USER,
+    "password": process.env.SQL_DB_PASS,
+    "database": process.env.SQL_DB_NAME,
+    "host": process.env.SQL_DB_HOST,
+    "dialect": process.env.SQL_DB_DIALECT,
     "operatorsAliases": false
   }
-}
+};
